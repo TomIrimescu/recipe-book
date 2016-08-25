a04ba6f97d0bc84717a141f6887ab8a18fb54aa5
TomIrimescu <tom@irimescu.com>
2016-08-24 20:30:04 -0400

    Added dropdown directive and misc mods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,14 @@ import { RecipesComponent } from "./recipes";
 import { RecipeListComponent } from "./recipes/recipe-list"
 import { RecipeItemComponent } from "./recipes/recipe-list"
 import { RecipeDetailComponent } from "./recipes/recipe-detail"
-import {ShoppingListComponent} from "./shopping-list";
-import {ShoppingListAddComponent} from "./shopping-list";
+import { ShoppingListComponent } from "./shopping-list";
+import { ShoppingListAddComponent } from "./shopping-list";
+import { DropdownDirective } from "./dropdown.directive";
 
 
 
 @NgModule({
-	declarations: [AppComponent, HeaderComponent, RecipesComponent, RecipeListComponent, RecipeItemComponent, RecipeDetailComponent, ShoppingListComponent, ShoppingListAddComponent],
+	declarations: [AppComponent, HeaderComponent, RecipesComponent, RecipeListComponent, RecipeItemComponent, RecipeDetailComponent, ShoppingListComponent, ShoppingListAddComponent, DropdownDirective],
 	imports: [BrowserModule, FormsModule ],
 	bootstrap: [AppComponent]
 })
